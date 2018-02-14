d7def6d1466f996cd5578e48dfc7113c5747ee63
Ash Abbas <aabbas6@masonlive.gmu.edu>
2018-02-14 17:43:01 -0500

    testing state

--- Digital Assignment 2/js/main.js
+++ Digital Assignment 2/js/main.js
@@ -12,30 +12,53 @@ window.onload = function()
     // All loading functions will typically all be found inside "preload()".
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    game.state.add('StateA', StateA);
+	game.state.add('StateB', StateB);
+	game.state.start('StateA');
+	
+	var StateA =
+    {
+		function preload() 
+		{
+			// Load an image
+			game.load.spritesheet( 'playButton', 'assets/PlayButton.png', 200, 100);
+		}
     
-    function preload() 
-	{
-        // Load an image
-		game.load.spritesheet( 'playButton', 'assets/PlayButton.png', 200, 100);
-    }
-    
-    var button;
+		var button;
 	
 	
-    function create() 
-	{
-		button = game.add.button(game.world.centerX-50,400,'playButton', actionOnClick, 2,1,0);
-			
-    }
+		function create() 
+		{
+			button = game.add.button(game.world.centerX-50,400,'playButton', actionOnClick, 2,1,0);
+		}
 	
-	function actionOnClick()
-	{
-		button.kill();
-	}
+		function actionOnClick()
+		{
+			game.state.start('StateB');
+		}
 	
-	function update()
-	{
+		function update()
+		{
 		
+		}
 	}
 	
+	var StateB =
+	{
+		function preload() 
+		{
+			// Load an image
+			game.load.spritesheet( 'playButton', 'assets/PlayButton.png', 200, 100);
+		}
+    
+		function create() 
+		{
+			game.stage.backgroundColor = '#182d3b';
+		}
+	
+		function update()
+		{
+		
+		}	
+	}
 };
