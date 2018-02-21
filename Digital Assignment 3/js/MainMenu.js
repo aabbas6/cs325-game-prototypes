31c9aa81a340ea8c0c70aebbf685e0403f64b53d
Ash Abbas <aabbas6@masonlive.gmu.edu>
2018-02-21 17:00:58 -0500

    add instructions

--- Digital Assignment 3/js/MainMenu.js
+++ Digital Assignment 3/js/MainMenu.js
@@ -3,7 +3,7 @@
 GameStates.makeMainMenu = function( game, shared ) {
 
 	var music = null;
-	var playButton = null;
+    var playButton = null;
     
     function startGame(pointer) 
     {
@@ -20,7 +20,10 @@ GameStates.makeMainMenu = function( game, shared ) {
             //	We've already preloaded our assets, so let's kick right into the Main Menu itself.
             //	Here all we're doing is playing some music and adding a picture and button
             //	Naturally I expect you to do something significantly better :)
-    
+            var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+            var instructions = game.add.text(200, 200, 'P1 Controls: W,A,S,D', style);
+            var instructions2 = game.add.text(400,200,'P2: Controls: Directional Keys', style);
+            var instructions3 = game.add.text(300,300,'Red Tags the other player',style);
             playButton = game.add.button(game.world.centerX-100,400, 'Button', startGame,2,1,0);    
         }
     };
