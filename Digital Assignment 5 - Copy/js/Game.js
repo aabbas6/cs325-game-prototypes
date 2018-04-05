f83d2d16fb312a6ca5d3a67e48b6f89aa7d48d33
Ash Abbas <aabbas6@masonlive.gmu.edu>
2018-04-05 12:50:56 -0400

    yay progress

--- Digital Assignment 5 - Copy/js/Game.js
+++ Digital Assignment 5 - Copy/js/Game.js
@@ -36,34 +36,43 @@ GameStates.makeGame = function( game, shared ) {
     var PickAxeLvM;
     var WeaponLvM;
     var ArmorLvM;
-
+    var pickAxeCost;
+    var axeCost;
+    var weaponCost;
+    var armorCost;
+    var menumusic;
+    var gatheringmusic;
+    var craftingmusic;
     return {
     
         create: function () 
         {
             game.physics.startSystem(Phaser.Physics.ARCADE);
+            game.stage.backgroundColor = "#A0522D" ;
             var style = { font: "30px Verdana", fill: "#9999ff", align: "center" };
+            var fount = { font: "15px Verdana", fill: "#9999ff", align: "center"};
+            menumusic = game.add.audio("Adventure");
+            craftingmusic = game.add.audio("Crafting");
+            gatheringmusic = game.add.audio("Gathering");
+            menumusic.play();
             //initialize
             craftButton = game.add.button(100, 400, 'craftButton', this.craftStart, this, 2,0,1);
             gatherButton = game.add.button(310, 400, 'gatherButton', this.gatherStart, this, 2,0,1);
             Ore = game.add.button(game.world.centerX - 200, game.world.centerY-100, 'ore', this.metalAdd,this);
             Tree = game.add.button(game.world.centerX + 100, game.world.centerY-100,'tree', this.woodAdd,this);
-            Axe = game.add.sprite(150,150, 'axe');
-            Axe.visible = false;
-            pickAxe = game.add.sprite(300,150,'pickaxe');
-            pickAxe.visible = false;
-            Armor = game.add.sprite(150,300, 'armor');
-            Armor.visible = false;
-            Weapon = game.add.sprite(300,300, 'weapon');
-            Weapon.visible = false;
+            Axe = game.add.button(150,200, 'axe',this.levelAxe,this);
+            pickAxe = game.add.button(150,400,'pickaxe',this.levelPickAxe,this);
+            Armor = game.add.button(450,200, 'armor',this.levelArmor,this);
+            Weapon = game.add.button(450,400, 'weapon',this.Weapon,this);
+
             //set Icons
-            pickaxeIcon = game.add.sprite(150,150,'pickaxe');
+            pickaxeIcon = game.add.sprite(150,100,'pickaxe');
             pickaxeIcon.scale.setTo(0.5,0.5);
-            axeIcon = game.add.sprite(300, 150, 'axe');
+            axeIcon = game.add.sprite(300, 100, 'axe');
             axeIcon.scale.setTo(0.5,0.5);
-            armorIcon = game.add.sprite(450,150,'armor');
+            armorIcon = game.add.sprite(450,100,'armor');
             armorIcon.scale.setTo(0.5,0.5);
-            weaponIcon = game.add.sprite(600,150,'weapon');
+            weaponIcon = game.add.sprite(600,100,'weapon');
             weaponIcon.scale.setTo(0.5,0.5);
     
             //create Stats
@@ -74,21 +83,17 @@ GameStates.makeGame = function( game, shared ) {
             WeaponLv = 1;
             ArmorLv = 1;
             DayCount = 1;
+           
             //set Message
             dayMessage1 = game.add.text(game.world.centerX - 40, 20, "Day: " + DayCount,style);
             metalCountM = game.add.text(120, 50, "Metal:" + metalCount, style);
             woodCountM = game.add.text(520, 50, "Wood: " + woodCount, style);
-            PickAxeLvM = game.add.text(200, 150, ": " + pickAxeLv,style);
-            AxeLvM = game.add.text(350,150, ": "+ AxeLv,style);
-            ArmorLvM = game.add.text(500,150, ": "+ ArmorLv,style);
-            WeaponLvM = game.add.text(650,150, ": "+ WeaponLv,style);
-            //create on Input Command
-           
-            //pickAxe.events.onInputDown.add(this.levelPickAxe,this);
-            //Axe.events.onInput.add(levelAxe,this);
-            //Armor.events.onInput.add(levelArmor,this);
-            //Weapon.events.onInput.add(levelWeapon,this);
-            //set them to false
+            PickAxeLvM = game.add.text(200, 100, ": " + pickAxeLv,style);
+            AxeLvM = game.add.text(350,100, ": "+ AxeLv,style);
+            ArmorLvM = game.add.text(500,100, ": "+ ArmorLv,style);
+            WeaponLvM = game.add.text(650,100, ": "+ WeaponLv,style);
+         
+            //set visibility them to false
             Ore.visible = false;
             Tree.visible = false;
             pickAxe.visible = false;
@@ -99,17 +104,26 @@ GameStates.makeGame = function( game, shared ) {
         ,
         craftStart: function()
         {
+            menumusic.stop();
+            craftingmusic.play();
             this.switchMenu();
+            game.time.events.add(Phaser.Timer.SECOND * 5, this.endCraft, this);
             pickAxe.visible = true;
             Axe.visible = true;
             Armor.visible = true;
             Weapon.visible = true;
+            axeCost = game.add.text(250,200, "Metal Cost: " + (AxeLv * 1) + "\nWood Cost: " + (AxeLv * 4));
+            pickAxeCost = game.add.text(250,400, "Metal Cost:  "+ (pickAxeLv * 2) + "\nWood Cost: " + (pickAxeLv * 3));
+            armorCost = game.add.text(550,200, "Metal Cost: " + (ArmorLv * 5));
+            weaponCost = game.add.text(550,400, "Metal Cost: " + (WeaponLv * 4) + "\nWoodCost: " + (WeaponLv * 1));
+
         },
         gatherStart: function()
         {
+            menumusic.stop();
+            gatheringmusic.play();
             this.switchMenu();
-
-            //game.time.events.add(Phaser.Timer.SECOND * 30, endGather, this); 
+            game.time.events.add(Phaser.Timer.SECOND * 3, this.endGather, this); 
             Ore.visible = true;
             Tree.visible = true;
             //Ore.inputEnabled = true;
@@ -118,28 +132,30 @@ GameStates.makeGame = function( game, shared ) {
           endCraft:function()
         {
             DayCount++;
+            dayMessage1.setText("Day: "+ DayCount);
 
-            switchMenu();
-
-            pickAxe.visible = true;
-            Axe.visible = true;
-            Armor.visible = true;
-            Weapon.visible = true;
-
-            pickAxe.inputEnabled = true;
-            Axe.inputEnabled = true;
-            Armor.inputEnabled = true;
-            Weapon.inputEnabled = true;
+            this.switchMenu();
+            axeCost.setText("");
+            pickAxeCost.setText("");
+            armorCost.setText("");
+            weaponCost.setText("");
+            pickAxe.visible = false;
+            Axe.visible = false;
+            Armor.visible = false;
+            Weapon.visible = false;
+            craftingmusic.stop();
+            menumusic.play();
         },
         endGather:function()
         {
             //day increment
             DayCount++;
-            switchMenu();
+            dayMessage1.setText("Day: "+ DayCount);
+            this.switchMenu();
             Ore.visible = false;
             Tree.visible = false;
-            Ore.inputEnabled = false;
-            Tree.inputEnabled = false;
+            gatheringmusic.stop();
+            menumusic.play();
         },
         metalAdd:function()
         {
@@ -151,14 +167,16 @@ GameStates.makeGame = function( game, shared ) {
             woodCount += AxeLv;
             woodCountM.setText(" Wood: "+ woodCount);
         },
-    /*
         levelPickAxe: function()
         {
             if( (metalCount>= (pickAxeLv * 2)) && (woodCount>= (pickAxeLv * 3)) )
             {
                 metalCount = metalCount - (pickAxeLv * 2);
+                metalCountM.setText("Metal: "+ metalCount);
                 woodCount = woodCount - (pickAxeLv * 3);
+                woodCountM.setText(" Wood: "+ woodCount);
                 pickAxeLv++;
+                PickAxeLvM.setText(": " + pickAxeLv);
             }
         },
         levelAxe: function()
@@ -172,9 +190,9 @@ GameStates.makeGame = function( game, shared ) {
         },
         levelWeapon: function()
         {
-            if( (metalCount>= (WeaponLv * 3)) && (woodCount>= (WeaponLv * 1)))
+            if( (metalCount>= (WeaponLv * 4)) && (woodCount>= (WeaponLv * 1)))
             {
-                metalCount = metalCount - (WeaponLv * 3);
+                metalCount = metalCount - (WeaponLv * 4);
                 woodCount = woodCount - (WeaponLv * 1);
                 weaponLv++;
             }
@@ -187,17 +205,8 @@ GameStates.makeGame = function( game, shared ) {
                    armorLv++;
             }
         },
-        */
         switchMenu: function()
         {
-            pickaxeIcon.visible =! pickaxeIcon.visible;
-            //pickAxeLvM.visible =! pickAxeLvM.visible;
-            axeIcon.visible =! axeIcon.visible;
-            //AxeLvM.visible =! AxeLvM.visible;
-            armorIcon.visible =! armorIcon.visible;
-            //ArmorLvM.visible =! ArmorLvM.visible;
-            weaponIcon.visible =! weaponIcon.visible;
-            //WeaponLvM.visible =! weaponLv.visible;
             craftButton.visible =! craftButton.visible;
             gatherButton.visible =! gatherButton.visible;
             craftButton.inputEnabled =! craftButton.inputEnabled;
--- Digital Assignment 5 - Copy/js/MainMenu.js
+++ Digital Assignment 5 - Copy/js/MainMenu.js
@@ -23,7 +23,7 @@ GameStates.makeMainMenu = function( game, shared ) {
             //	Here all we're doing is playing some music and adding a picture and button
             //	Naturally I expect you to do something significantly better :)
             var style = { font: "15px Verdana", fill: "#9999ff", align: "center" };
-            var instructions = game.add.text(200, 100, 'You have 30 days left until the world ends!', style);
+            var instructions = game.add.text(200, 100, 'You have 10 days left until the world ends!', style);
             var instructions2 = game.add.text(200,200,'Gather Materials, Craft those Materials, and Defeat the Villain!', style);
             playButton = game.add.button(game.world.centerX-100,400, 'Button', startGame,1,2,0);   
 
